e6e9dbebb5cd0436f0b023a2bc69e8137b6a06a6
Rodrigo <rodrigolemosrl@hotmail.com>
2020-10-15 19:31:54 -0300

    Creating migration for products table

---
+++ src/shared/infra/typeorm/migrations/1602799380337-CreateProducts.ts
@@ -0,0 +1,80 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateProducts1602799380337 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'products',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'description',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'brand',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'provider',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'classification',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'cost_price',
+                        type: 'decimal',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'sale_price',
+                        type: 'decimal',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'stock_quantity',
+                        type: 'integer',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'store_quantity',
+                        type: 'integer',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('products');
+    }
+
+}
