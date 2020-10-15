8f8f289a59b0af8dbd802ab4891ee8fac9df5b13
Rodrigo <rodrigolemosrl@hotmail.com>
2020-10-15 20:14:41 -0300

    Using ListProductService in ProductsController

--- src/modules/products/infra/http/controllers/ProductsController.ts
+++ src/modules/products/infra/http/controllers/ProductsController.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from 'express';
 
+import ListProductService from '@modules/products/services/ListProductsService';
+
 export default class ProductsController {
 
   public async get(req: Request, res: Response) {
-    return res.json({
-      hello: 'list'
-    });
+    
+    const listProductService = new ListProductService();
+
+    const productsFound = await listProductService.execute();
+
+    return res.json(productsFound);
   }
 
   public async create(req: Request, res: Response) {
