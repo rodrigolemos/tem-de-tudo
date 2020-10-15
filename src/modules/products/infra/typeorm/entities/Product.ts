ccd33d0cd811fb88e884a237200b500c01572028
Rodrigo <rodrigolemosrl@hotmail.com>
2020-10-15 19:39:36 -0300

    Creating product model

---
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -0,0 +1,48 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('products')
+class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  brand: string;
+
+  @Column()
+  provider: string;
+
+  @Column()
+  classification: string;
+
+  @Column()
+  cost_price: number;
+
+  @Column()
+  sale_price: number;
+
+  @Column()
+  stock_quantity: number;
+
+  @Column()
+  store_quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Product;
