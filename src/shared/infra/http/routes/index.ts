1b8311c8d9ff82c39e679c49ae897649db7693c8
Rodrigo <rodrigolemosrl@hotmail.com>
2020-10-16 20:47:39 -0300

    CHECKPOINT 3 - Creating sales migration

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import productsRouter from '@modules/products/routes/products.routes';
 import partnersRouter from '@modules/partners/routes/partners.routes';
+import salesRouter from '@modules/sales/routes/sales.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/partners', partnersRouter);
+routes.use('/sales', salesRouter);
 
 export default routes;
---
+++ src/shared/infra/typeorm/migrations/1602890952000-CreateSales.ts
@@ -0,0 +1,93 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class CreateSales1602890952000 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'sales',
+                columns: [
+                    {
+                        name: 'order',
+                        type: 'integer',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    {
+                        name: 'product_id',
+                        type: 'integer',
+                        isPrimary: true,
+                        isNullable: false,
+                    },
+                    {
+                        name: 'quantity',
+                        type: 'integer',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'sale_price',
+                        type: 'decimal',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'customer_id',
+                        type: 'integer',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'seller_id',
+                        type: 'integer',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'date',
+                        type: 'timestamp',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
+
+        await queryRunner.createForeignKey(
+            'sales',
+            new TableForeignKey({
+                name: 'SalesProduct',
+                columnNames: ['product_id'],
+                referencedColumnNames: ['id'],
+                referencedTableName: 'products',
+                onDelete: 'SET NULL',
+                onUpdate: 'CASCADE',
+            }),
+        );
+
+        await queryRunner.createForeignKey(
+            'sales',
+            new TableForeignKey({
+                name: 'SalesPartner',
+                columnNames: ['customer_id', 'seller_id'],
+                referencedColumnNames: ['id'],
+                referencedTableName: 'partners',
+                onDelete: 'SET NULL',
+                onUpdate: 'CASCADE',
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey('products', 'SalesProduct');
+        await queryRunner.dropForeignKey('partners', 'SalesPartner');
+        await queryRunner.dropTable('sales');
+    }
+
+}
